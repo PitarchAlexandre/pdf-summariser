5c8b1b48d2db873b8a1356b4f8c7656e5fe6ecac
Pitarch Alexandre <alexandre.pitarch@gmail.com>
2025-02-06 00:28:31 +0100

    OCR in construction...
    Faut faire le trie de ce qui marche et marche pas dans toute c'est bibliothèques

--- PDF-Summariser/ia_client.py
+++ PDF-Summariser/ia_client.py
@@ -24,22 +24,23 @@ class IAClient:
             raise ValueError(f"Erreur lors de l'initialisation du client OpenAI : {e}")
     
     def parameter_prompt(self, number_max_word, document, language):
-
-        if number_max_word > 0:
-            number_word = f" en maximum {number_max_word} mots "
-        else:
-            number_word=""
-        prompt=[        
-                    {"role": "system", "content": f"""You are a bot which translate texts. You receive texts. Your sole aim is to make summaries. 
-                                                    These summaries should help you understand the text."You receive texts that were previously in PDF files,
-                                                    but have now been converted into text files. If there are chapters, you don't have to name them one by one,
-                                                    but summarize the whole text. You must not give your opinion or interpret in your own way.
-                                                    You have to start your sentence by "Here is a word summary in {number_word} words" in {language}.
-                                                    You must not exceed the word count. Do not write more than {number_word}""" },
-                    {"role": "user", "content": f"Summarize the following text in maximum {number_word} words. :\n{document}." }
-        ]
-        return prompt
-
+        try:
+            if number_max_word > 0:
+                number_word = f" en maximum {number_max_word} mots "
+            else:
+                number_word=""
+            prompt=[        
+                        {"role": "system", "content": f"""You are a bot which translate texts. You receive texts. Your sole aim is to make summaries. 
+                                                        These summaries should help you understand the text."You receive texts that were previously in PDF files,
+                                                        but have now been converted into text files. If there are chapters, you don't have to name them one by one,
+                                                        but summarize the whole text. You must not give your opinion or interpret in your own way.
+                                                        You have to start your sentence by "Here is a word summary in {number_word} words" in {language}.
+                                                        You must not exceed the word count. Do not write more than {number_word}""" },
+                        {"role": "user", "content": f"Summarize the following text in maximum {number_word} words. :\n{document}." }
+            ]
+            return prompt
+        except Exception as e:
+            ValueError(f"Erreur de l'initianlisation du prompt : {e}")
 
     def generate_summary(self, client, ia_model, prompt, text_box):
         stream = client.chat.completions.create(
--- PDF-Summariser/pdf.py
+++ PDF-Summariser/pdf.py
@@ -8,6 +8,7 @@ from PyPDF2 import PdfFileReader
 from config_ocr import OcrConfig
 import ocrmypdf
 import requests
+import aspose.pdf as ap
 
 #https://docs.python.org/3/library/tkinter.messagebox.html
 #https://github.com/py-pdf/pypdf
@@ -17,7 +18,8 @@ import requests
 #https://jonathansoma.com/everything/pdfs/ocr-tools/
 #https://ocr.space/ocrapi 
 #https://www.docstring.fr/formations/faq/fichiers/comment-manipuler-des-fichiers-json-en-python/
-    
+#https://docs.aspose.com/pdf/python-net/optimize-pdf/#reduce-size-pdf
+
 class PdfFile():
 
     # Open the file explorateur to choose a PDF file
@@ -32,10 +34,27 @@ class PdfFile():
         # Get the api key from the json file (config_ocr.json)
         print("Function ocr to text")
         api_key = OcrConfig("config_ocr.json").get_default_key
+        print(api_key)
 
         try:
-            payload = {'isOverlayRequired': True,
-                    'apikey': 'api_key',
+            # Open document
+            document = ap.Document(pdf_file)
+            # Optimize PDF document. Note, though, that this method cannot guarantee document shrinking
+            document.optimize_resources()
+            # Initialize OptimizationOptions
+            optimizeOptions = ap.optimization.OptimizationOptions()
+            # Set CompressImages option
+            optimizeOptions.image_compression_options.compress_images = True
+            # Set ImageQuality option
+            optimizeOptions.image_compression_options.image_quality = 50
+            # Optimize PDF document using OptimizationOptions
+            document.optimize_resources(optimizeOptions)
+            # Save updated document
+            document.save(pdf_file)
+
+
+            payload = {'isOverlayRequired': False,
+                    'apikey': 'helloworld',
                     'language': language,
                     }
             with open(pdf_file, 'rb') as f:
@@ -44,7 +63,25 @@ class PdfFile():
                                 data=payload,
                                 )
             r.content.decode()
-            if r.content.decode() == ''
+            print(r.json)
+
+            if r.content.decode() == 'The API key is invalid':
+                renderer = ChromePdfRenderer()
+                pdf_file = renderer.RenderUrlAsPdf(pdf_file)
+                
+                payload = {'isOverlayRequired': False,
+                    'apikey': 'helloworld',
+                    'language': language,
+                    }
+                with open(pdf_file, 'rb') as f:
+                    r = requests.post('https://api.ocr.space/parse/image',
+                                files={pdf_file: f},
+                                data=payload,
+                                )
+                return r.content.decode()
+            else:
+                return r.content.decode()
+            
         except Exception as e:
             print(f"Erreur : {e}")
         
@@ -77,7 +114,7 @@ class PdfFile():
         tk.messagebox.showwarning(title="Infos", message="Les fichiers doivent contenir un maximum de 1400 mots")
         try:
             warning_message = ""
-            while True:
+            while False:
                 # l'avantage de la fonction askinteger, c'est qu'elle controle directement que le nombre soit un int et évite les mauvaises entrées
                 number_max_word = simpledialog.askinteger(title="Nombre de mots", prompt=f"{warning_message}Inserérez le nombre maximum de mots du résumé : ")
                 if number_max_word >= 0 and number_max_word <= 1400:   
